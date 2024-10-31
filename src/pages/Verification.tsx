dab92eec9bc813dfc224201ab3186e796e67b667
VishnuSusilkumar <vishnususilkumar10@gmail.com>
2024-10-31 11:15:43 +0530

    feat: added title

--- src/pages/Verification.tsx
+++ src/pages/Verification.tsx
@@ -110,7 +110,7 @@ const Verification: React.FC = () => {
           <div className="flex justify-center">
             <button
               type="submit"
-              className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 flex items-center justify-center"
               disabled={otp.length < 4 || loading}
             >
               {loading ? <SubLoader /> : "Verify Account"}
